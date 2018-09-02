d25259c667ed24bf238365d73fab417f474433e3
Abdullah Fadel <abdullahfadel@std.sehir.edu.tr>
2018-09-02 14:42:52 +0300

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.dialects.postgresql import HSTORE
-from sqlalchemy.ext.mutable import MutableDict
-from app import   DB
-
-class Word_emddings(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    name = DB.Column(DB.String(80), nullable=False, unique=True)
-    stats = DB.Column(MutableDict.as_mutable(HSTORE))
