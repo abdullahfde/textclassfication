a8734ac8e03e75848bdc902b1940151c6c45af78
Abdullah Fadel <abdullahfadel@std.sehir.edu.tr>
2018-09-02 14:43:04 +0300

    Delete data_process_and_classification.py

--- data_process_and_classification.py
+++
@@ -1,6 +0,0 @@
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-1) I have collected the data from many website by using beautifulsoap then I created two file, the first for names which
-called isim.txt and second one is  
-
-
-"""""
